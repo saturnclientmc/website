0b677ad12e12ab356711d6108834a668be6f2ebe
Leo dev <klesti.selimay@icloud.com>
2025-04-17 19:24:16 +0200

    Discord

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 import { Github, Instagram } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
+import Discord from "./Discord";
 
 export default function Footer() {
   return (
-    <footer className="text-white backdrop-blur-md py-4 flex flex-row">
-      <div className="flex flex-row gap-4 ml-4 absolute">
+    <div className="text-white py-4 flex w-max px-5 rounded-xl backdrop-blur-md mx-auto mb-3 fade-in-up">
+      <div className="flex flex-row gap-4">
         <Link href="https://github.com/saturnclientmc" target="_blank">
           <Github className="transition-colors duration-300 hover:text-[#e2bf7d] animate-fadeIn" />
         </Link>
@@ -14,19 +14,10 @@ export default function Footer() {
           <Instagram className="transition-colors duration-300 hover:text-[#e2bf7d] animate-fadeIn" />
         </Link>
 
-        <Link href="https://www.instagram.com/saturnclient/" target="_blank">
-          <Image
-            src="/discord.svg"
-            alt="Discord"
-            className="transition-colors duration-300 hover:text-[#e2bf7d] animate-fadeIn"
-          />
+        <Link href="https://discord.gg/HFRsNxfCqT" target="_blank">
+          <Discord className="transition-colors duration-300 hover:text-[#e2bf7d] animate-fadeIn size-[24px]" />
         </Link>
       </div>
-
-      <div className="flex flex-row w-max mx-auto gap-1">
-        <p className="font-[mono]">&copy;</p> 2025 Saturn Client. All rights
-        reserved.
-      </div>
-    </footer>
+    </div>
   );
 }
